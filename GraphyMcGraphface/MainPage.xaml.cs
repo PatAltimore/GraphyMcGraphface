d1c64ba659e650fb5c08c52ea934757282067f0a
Pat Altimore <patricka@microsoft.com>
2016-07-14 16:18:49 -0700

    Initial commit

---
+++ GraphyMcGraphface/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GraphyMcGraphface
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnGetData_Click(object sender, RoutedEventArgs e)
+        {
+            // Set the client id to the registered application Graphy McGraphface
+            PublicClientApplication pca = new PublicClientApplication("ac8ce5fc-e6d8-437d-9254-6d4cba2e2d1f");
+
+            // Define permission scope to read mail
+            string[] scopes = new string[] { "Mail.Read" };
+
+            // Graph request URL to get "my" messages
+            string apiURL = "https://graph.microsoft.com/beta/me/messages";
+
+            // Get the authorization token
+            AuthenticationResult ar = await pca.AcquireTokenAsync(scopes);
+
+            // Set up HTTP request
+            HttpClient client = new HttpClient();
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, apiURL);
+
+            // Add the authorization token to the HTTP header of the request
+            message.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", ar.Token);
+
+            // Make HTTP request and get the response
+            HttpResponseMessage response = await client.SendAsync(message);
+
+            // Display the response message in the text box.
+            txtResults.Text = await response.Content.ReadAsStringAsync();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # GraphyMcGraphface
 Super simple Microsoft Graph API UWP app to illustrate basic requirements.
+
+This sample is based on the demo highlighted in this video: https://channel9.msdn.com/Shows/Office-Dev-Show/Office-Dev-Show-Episode-27-Azure-AD-Converged-Authentication-and-the-Microsoft-Graph
+
+## Suggested demo steps
+
+### Registration 
+1.	Show client id in the registration portal https://apps.dev.microsoft.com/#/appList  
+1.	Show same tenet has the app registered in the management portal too. http://manage.windowsazure.com/
+1.	Open Graphy app. 
+1.   Show it uses the MSAL nuget package
+1.	Show PublicClientApplication object created using client ID. 
+### Permissions
+1.	MSAL library and Azure AD V2 endpoint allows us to define the permission scopes programmatically.
+1.	We can see that we only need the scope “Mail.Read”.  If I looked at the documentation, it would show me the permission scopes required for a request.
+### Get token
+1.	Use the PublicClientApplication object to acquire an access token.  
+1.	We need the token to add it to the HTTP header of our request.
+### Make  REST request
+1.	Use .NET’s HTTP Client class to do a HTTP request & response.  This is our REST call to the endpoint.
+### Run the code:
+1.	Use a test Office 365 account. 
+
